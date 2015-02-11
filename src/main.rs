4769e665f584b7cc756cd3cd4d4368dffa4b8a14
ReanGD <reangd@yandex.ru>
2015-02-12 01:54:01 +0300

    if

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let x = 5*2;
-    let y: f32 = 55.0;
-    let (a, b) = (1, 55.9);
-    println!("x = {}; y = {}; a = {}; b = {}", x, y, a, b);
+    let x = 5;
+    let y = if x == 5 {50} else {60};
+    println!("x = {}; y = {};", x, y);
 }
