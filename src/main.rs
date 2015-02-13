d2799ba78dc23e97f23ce4a7aa37b84e6321d95a
ReanGD <reangd@yandex.ru>
2015-02-13 03:07:30 +0300

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-fn main() {
-    let (x,y) = (1,2);
-    println!("x,y=({},{})", x,y);
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
+fn sum(x: Vec3, y: Vec3) -> Vec3 {
+    Vec3{x: x.x + y.x, y: x.y + y.y, z: x.z + y.z}
+}
+
+fn main() {
+    let a = Vec3{x: 1.0, y: 2.0, z: 3.0};
+    let b = Vec3{x: 4.0, y: 5.0, z: 6.0};
+    let c = sum(a, b);
+    println!("a+b=({}, {}, {})", c.x, c.y, c.z);
+}
