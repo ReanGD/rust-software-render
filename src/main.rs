387a7920c870b39c4a56aa08aa892dbafc61e1e0
ReanGD <reangd@yandex.ru>
2015-08-02 02:33:02 +0300

    draw 16000 triangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate rand;
 extern crate sdl2;
 extern crate cgmath;
 
@@ -5,22 +6,38 @@ mod device;
 mod rasterization;
 mod tests;
 
-use device::Device;
 use cgmath::Point2;
-use rasterization::triangle;
+use rand::distributions::{IndependentSample, Range};
 
+use device::Device;
+use rasterization::triangle;
 
 pub fn main() {
     let mut device = Device::new("rust software render", 800, 600);
 
+    let color_range = Range::new(0, std::u32::MAX);
+    let x_range = Range::new(-50f32, device.x_size as f32 + 50f32);
+    let y_range = Range::new(-50f32, device.y_size as f32 + 50f32);
+    let mut rng = rand::thread_rng();
+
+    let cnt = 16000;
+    let mut points: Vec<Point2<f32>> = vec![];
+    let mut colors: Vec<u32> = vec![];
+    for _ in 0..cnt {
+        points.push(Point2::new(x_range.ind_sample(&mut rng), y_range.ind_sample(&mut rng)));
+        points.push(Point2::new(x_range.ind_sample(&mut rng), y_range.ind_sample(&mut rng)));
+        points.push(Point2::new(x_range.ind_sample(&mut rng), y_range.ind_sample(&mut rng)));
+        colors.push(color_range.ind_sample(&mut rng));
+    }
+    
     while device.keyboard() {
         device.clear(0xFFFFFF);
-        triangle(&mut device.cbuffer,
-                 device.x_size,
-                 device.y_size,
-                 Point2::new(100.0_f32, 300.0_f32),
-                 Point2::new(400.0_f32, 100.0_f32),
-                 Point2::new(500.0_f32, 200.0_f32));
+        for i in 0..cnt {
+            triangle(&mut device.cbuffer,
+                     device.x_size,
+                     device.y_size,
+                     points[i*3 + 0], points[i*3 + 1], points[i*3 + 2], colors[i]);
+        }
         device.present();
         device.draw_fps();
     }
