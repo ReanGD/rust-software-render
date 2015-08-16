99d5c12f611d019ab3d4a9ce273a4ab61a461f3d
ReanGD <reangd@yandex.ru>
2015-08-16 12:30:21 +0300

    generate sphere

--- src/generator.rs
+++ src/generator.rs
@@ -1,9 +1,10 @@
+use std;
 use cgmath::*;
 use mesh::{Vertex, Mesh, Model};
 
 pub fn generate_plane() -> Result<Model, String> {
-    let mut mesh = Mesh::new();
     let mut model = Model::new();
+    let mut mesh = Mesh::new();
     let mut vb: Vec<Vertex> = vec![Vertex::new(); 4];
     vb[0].position = Vector3::new(-0.5_f32,  0.5_f32, 0.0_f32);
     vb[0].normal   = Vector3::new( 0.0_f32,  0.0_f32, 1.0_f32);
@@ -66,4 +67,77 @@ pub fn generate_cube() -> Result<Mesh, String> {
     try!(mesh.index(ib));
 
     Ok(mesh)
+}
+
+pub fn generate_sphere(cnt_vertex: usize) -> Result<Model, String> {
+    let mut model = Model::new();
+    let mut mesh = Mesh::new();
+
+	let plg = cnt_vertex / 2 - 1;
+	let vertex_cnt = plg * cnt_vertex + 2;
+	let index_cnt = 6 * (cnt_vertex - 1) * plg;
+    
+    let mut vb: Vec<Vertex> = vec![Vertex::new(); vertex_cnt];
+    let mut angle_b = rad(-std::f32::consts::PI / 2.0_f32);
+	let step_a = rad(std::f32::consts::PI * 2.0_f32 / (cnt_vertex as f32 - 1.0_f32));
+	let step_b = rad(std::f32::consts::PI / (plg as f32 + 1.0_f32));
+    let mut ind = 1;
+    for _ in 0 .. plg {
+		angle_b = angle_b + step_b;
+        let (y, radius) = sin_cos(angle_b);
+
+        let mut angle_a = rad(0.0_f32);
+        for _ in 0 .. cnt_vertex {
+            let (sin_a, cos_a) = sin_cos(angle_a);
+			vb[ind].position = Vector3::new(radius * cos_a, y, radius * sin_a);
+			vb[ind].normal   = Vector3::new(radius * cos_a, y, radius * sin_a).normalize();
+            ind += 1;
+            angle_a = angle_a + step_a;
+		}
+	}
+	vb[0].position = Vector3::new(0.0_f32, -1.0_f32, 0.0_f32);
+	vb[0].normal = Vector3::new(0.0_f32, -1.0_f32, 0.0_f32);
+	vb[vertex_cnt - 1].position = Vector3::new(0.0_f32, 1.0_f32, 0.0_f32);
+	vb[vertex_cnt - 1].normal = Vector3::new(0.0_f32, 1.0_f32, 0.0_f32);
+
+    mesh.vertex(vb);
+
+    let mut ib: Vec<u32> = vec![0; index_cnt];
+    let mut ind = 0;
+    for ix in 0 .. (plg-1) {
+		let mut z1 = ix * cnt_vertex + 1;
+        let mut z2 = z1 + 1;
+		let mut z3 = z1 + cnt_vertex;
+        let mut z4 = z3 + 1;
+		for _ in 0 .. (cnt_vertex - 1) {
+			ib[ind + 0] = z1 as u32;
+            ib[ind + 1] = z2 as u32;
+            ib[ind + 2] = z3 as u32;
+            
+			ib[ind + 3] = z4 as u32;
+            ib[ind + 4] = z3 as u32;
+            ib[ind + 5] = z2 as u32;
+            z1 += 1;
+            z2 += 1;
+            z3 += 1;
+            z4 += 1;
+            ind += 6;
+		}
+	}
+    
+	// let iy = cnt_vertex * (plg - 1);
+	// for ix in 1 .. cnt_vertex {
+	// 	ib[ind + 0] = ix as u32;
+    //     ib[ind + 1] = (ix + 1) as u32;
+    //     ib[ind + 2] = 0 as u32;
+	// 	ib[ind + 3] = (iy + ix + 1) as u32;
+    //     ib[ind + 4] = (iy + ix) as u32;
+    //     ib[ind + 5] = (vertex_cnt - 1) as u32; 
+    //     ind += 6;
+	// }
+    try!(mesh.index(ib));
+    mesh.calc_normal();
+    model.add(mesh);
+
+    Ok(model)
 }
