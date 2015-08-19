5d0d6ff8489fc7ed895be0e729b4bc4f0fbff4d5
ReanGD <reangd@yandex.ru>
2015-08-19 12:01:28 +0300

    rasterization optimization

--- src/rasterization.rs
+++ src/rasterization.rs
@@ -4,6 +4,8 @@ use cgmath::*;
 use shader::{Shader, MAX_OUT_VALUES};
 use memory::vector3_to_u32;
 
+const EPSILON: f32 = 0.0001_f32;
+
 #[inline]
 fn sort_by_y<'a>(points: &'a[Point3<f32>; 3], vertex_data: &'a[[f32;MAX_OUT_VALUES]; 3]) ->
     (&'a Point3<f32>, &'a Point3<f32>, &'a Point3<f32>, &'a [f32;MAX_OUT_VALUES], &'a [f32;MAX_OUT_VALUES], &'a [f32;MAX_OUT_VALUES]) {
@@ -31,6 +33,92 @@ fn sort_by_y<'a>(points: &'a[Point3<f32>; 3], vertex_data: &'a[[f32;MAX_OUT_VALU
         (a, b, c, va, vb, vc)
     }
 
+#[inline]
+fn half_triangle(cbuffer: &mut Vec<u32>,
+                 zbuffer: &mut Vec<f32>,
+                 x_size: usize,
+                 vertex_data_cnt: usize,
+                 shader: &mut Shader,
+                 point_base: &Point3<f32>,
+                 vd_base: &[f32;MAX_OUT_VALUES],
+                 y_begin: usize,
+                 y_end: usize,
+                 step0: &[f32; MAX_OUT_VALUES + 2],
+                 step1: &[f32; MAX_OUT_VALUES + 2]
+                 ) {
+    let mut vdata0_step = [0.0_f32; MAX_OUT_VALUES];
+    let mut vdata0      = [0.0_f32; MAX_OUT_VALUES];
+    let mut dvdata_step = [0.0_f32; MAX_OUT_VALUES];
+    let mut dvdata      = [0.0_f32; MAX_OUT_VALUES];
+    let mut vdata_step  = [0.0_f32; MAX_OUT_VALUES];
+    let mut vdata       = [0.0_f32; MAX_OUT_VALUES];
+
+    let pixel_func = shader.pixel_func;
+    let y_step = y_begin as f32 + 0.5_f32 - point_base.y;
+    let x0_step = step0[0];
+    let x1_step = step1[0];
+    let z0_step = step0[1];
+    let z1_step = step1[1];
+
+    let mut x1 = point_base.x + y_step * x0_step + 0.5_f32 - EPSILON;
+    let mut z1 = point_base.z + y_step * z0_step; // inverse z
+    let mut x2 = point_base.x + y_step * x1_step + 0.5_f32 - EPSILON;
+
+    let dx_step = x0_step - x1_step;
+    let dz_step = z0_step - z1_step;
+
+    let mut dx = y_step * dx_step;
+    let mut dz = y_step * dz_step;
+
+    for ind in 0..vertex_data_cnt {
+        vdata0_step[ind] = step0[ind + 2];
+        let vdata1_step = step1[ind + 2];
+
+        vdata0[ind] = vd_base[ind] + y_step * vdata0_step[ind];
+        dvdata_step[ind] = vdata0_step[ind] - vdata1_step;
+        dvdata[ind] = y_step * dvdata_step[ind];
+    }
+
+    let mut offset = y_begin * x_size;
+    for _ in y_begin..y_end {
+        let x1_int = cmp::min(cmp::max(x1 as i32, 0) as usize, x_size - 1);
+        let x2_int = cmp::min(cmp::max(x2 as i32, 0) as usize, x_size);
+        if x2_int > x1_int {
+            let z_step = dz / dx;
+            let mut z = z1 + z_step * (x1_int as f32 - x1 - EPSILON); // inverse z
+            for ind in 0..vertex_data_cnt {
+                vdata_step[ind] = dvdata[ind] / dx;
+                vdata[ind] = vdata0[ind] + vdata_step[ind] * (x1_int as f32 - x1 - EPSILON);
+            }
+
+            for x in x1_int..x2_int {
+                z += z_step;
+                for ind in 0..vertex_data_cnt {
+                    vdata[ind] += vdata_step[ind];
+                }
+                if zbuffer[offset + x] < z {
+                    for ind in 0..vertex_data_cnt {
+                        shader.in_pixel_data[ind] = vdata[ind] / z;
+                    }
+                    cbuffer[offset + x] = vector3_to_u32(&pixel_func(shader));
+                    zbuffer[offset + x] = z;
+                }
+            }
+        }
+        offset += x_size;
+        x1 += x0_step;
+        x2 += x1_step;
+        dx += dx_step;
+        z1 += z0_step;
+        dz += dz_step;
+        for ind in 0..vertex_data_cnt {
+            vdata0[ind] += vdata0_step[ind];
+            dvdata[ind] += dvdata_step[ind];
+        }
+    }
+}
+
+#[inline]
 pub fn triangle(cbuffer: &mut Vec<u32>,
                 zbuffer: &mut Vec<f32>,
                 x_size: usize,
@@ -53,10 +141,8 @@ pub fn triangle(cbuffer: &mut Vec<u32>,
         (a.x > max_x && b.x > max_x && c.x > max_x) {
             return;
         }
-    
-    // steps for line
-    const EPSILON: f32 = 0.0001_f32;
 
+    // steps for line
     let mut step_ab = [0.0_f32; MAX_OUT_VALUES + 2];
     let inv_dy_ab = if a.y - b.y > EPSILON {
         1.0_f32 / (a.y - b.y)
@@ -91,15 +177,10 @@ pub fn triangle(cbuffer: &mut Vec<u32>,
         step_bc[i + 2] = (vb[i] - vc[i]) * inv_dy_bc;
     }
     
-
     // y-ranges: [y0; y1) + [y1; y2)
     let y0 = cmp::min(cmp::max((c.y + 0.5_f32) as i32, 0) as usize, y_size - 1);
     let y1 = cmp::min(cmp::max((b.y + 0.5_f32) as i32, 0) as usize, y_size);
     let y2 = cmp::min(cmp::max((a.y + 0.5_f32) as i32, 0) as usize, y_size);
-    let point_base = [c, a];
-    let vd_base = [vc, va];
-    let y_begin = [y0, y1];
-    let y_end = [y1, y2];
 
     let step0 = if step_bc[0] > step_ac[0] {
         (&step_ac, &step_bc)
@@ -111,80 +192,27 @@ pub fn triangle(cbuffer: &mut Vec<u32>,
     } else {
         (&step_ac, &step_ab)
     };
-    let steps = [step0, step1];
-    let pixel_func = shader.pixel_func;
 
-    let mut vdata0_step = [0.0_f32; MAX_OUT_VALUES];
-    let mut vdata0      = [0.0_f32; MAX_OUT_VALUES];
-    let mut dvdata_step = [0.0_f32; MAX_OUT_VALUES];
-    let mut dvdata      = [0.0_f32; MAX_OUT_VALUES];
-    let mut vdata_step  = [0.0_f32; MAX_OUT_VALUES];
-    let mut vdata       = [0.0_f32; MAX_OUT_VALUES];
-
-    for i in (0..2) {
-        if y_begin[i] < y_end[i] {
-            let y_step = y_begin[i] as f32 + 0.5_f32 - point_base[i].y;
-            let x0_step = steps[i].0[0];
-            let x1_step = steps[i].1[0];
-            let z0_step = steps[i].0[1];
-            let z1_step = steps[i].1[1];
-
-            let mut x1 = point_base[i].x + y_step * x0_step + 0.5_f32 - EPSILON;
-            let mut z1 = point_base[i].z + y_step * z0_step; // inverse z
-            let mut x2 = point_base[i].x + y_step * x1_step + 0.5_f32 - EPSILON;
-
-            let dx_step = x0_step - x1_step;
-            let dz_step = z0_step - z1_step;
-
-            let mut dx = y_step * dx_step;
-            let mut dz = y_step * dz_step;
-
-            for ind in 0..vertex_data_cnt {
-                vdata0_step[ind] = steps[i].0[ind + 2];
-                let vdata1_step = steps[i].1[ind + 2];
-
-                vdata0[ind] = vd_base[i][ind] + y_step * vdata0_step[ind];
-                dvdata_step[ind] = vdata0_step[ind] - vdata1_step;
-                dvdata[ind] = y_step * dvdata_step[ind];
-            }
-
-            let mut offset = y_begin[i] * x_size;
-            for _ in y_begin[i]..y_end[i] {
-                let x1_int = cmp::min(cmp::max(x1 as i32, 0) as usize, x_size - 1);
-                let x2_int = cmp::min(cmp::max(x2 as i32, 0) as usize, x_size);
-                if x2_int > x1_int {
-                    let z_step = dz / dx;
-                    let mut z = z1 + z_step * (x1_int as f32 - x1 - EPSILON); // inverse z
-                    for ind in 0..vertex_data_cnt {
-                        vdata_step[ind] = dvdata[ind] / dx;
-                        vdata[ind] = vdata0[ind] + vdata_step[ind] * (x1_int as f32 - x1 - EPSILON);
-                    }
-
-                    for x in x1_int..x2_int {
-                        z += z_step;
-                        for ind in 0..vertex_data_cnt {
-                            vdata[ind] += vdata_step[ind];
-                        }
-                        if zbuffer[offset + x] < z {
-                            for ind in 0..vertex_data_cnt {
-                                shader.in_pixel_data[ind] = vdata[ind] / z;
-                            }
-                            cbuffer[offset + x] = vector3_to_u32(&pixel_func(shader));
-                            zbuffer[offset + x] = z;
-                        }
-                    }
-                }
-                offset += x_size;
-                x1 += x0_step;
-                x2 += x1_step;
-                dx += dx_step;
-                z1 += z0_step;
-                dz += dz_step;
-                for ind in 0..vertex_data_cnt {
-                    vdata0[ind] += vdata0_step[ind];
-                    dvdata[ind] += dvdata_step[ind];
-                }
-            }
-        }
+    if y0 < y1 {
+        half_triangle(cbuffer,
+                      zbuffer,
+                      x_size,
+                      vertex_data_cnt,
+                      shader,
+                      c, vc, y0, y1,
+                      step0.0,
+                      step0.1
+                      );
+    }
+    if y1 < y2 {
+        half_triangle(cbuffer,
+                      zbuffer,
+                      x_size,
+                      vertex_data_cnt,
+                      shader,
+                      a, va, y1, y2,
+                      step1.0,
+                      step1.1
+                      );
     }
 }
--- src/shader.rs
+++ src/shader.rs
@@ -67,14 +67,19 @@ impl Shader {
     }
 
     fn read_vec4(&self, sm: usize) -> Vector4<f32> {
-        // Vector4::from_fixed_ref(self.in_vertex_data.slice(sm, sm+4))
         Vector4::new(self.in_vertex_data[sm + 0],
                      self.in_vertex_data[sm + 1],
                      self.in_vertex_data[sm + 2],
                      self.in_vertex_data[sm + 3])
     }
 
-    fn out_vec3(&mut self, val: &Vector3<f32>) {
+    fn read_vec3(&self, sm: usize) -> Vector3<f32> {
+        Vector3::new(self.in_vertex_data[sm + 0],
+                     self.in_vertex_data[sm + 1],
+                     self.in_vertex_data[sm + 2])
+    }
+
+    fn out_vec3_from4(&mut self, val: &Vector4<f32>) {
         self.out_vertex_data[self.vertex_out_len + 0] = val.x;
         self.out_vertex_data[self.vertex_out_len + 1] = val.y;
         self.out_vertex_data[self.vertex_out_len + 2] = val.z;
@@ -93,7 +98,7 @@ impl Shader {
         let pos = self.matrix_arr[MATRIX_PROJ_VIEW_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_POS));
         let norm = self.matrix_arr[MATRIX_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_NORM)).normalize();
 
-        self.out_vec3(&Vector3::new(norm.x, norm.y, norm.z));
+        self.out_vec3_from4(&norm);
         pos
     }
 
@@ -139,8 +144,8 @@ impl Shader {
         let norm = self.matrix_arr[MATRIX_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_NORM)).normalize();
         let view = self.read_vec4(IN_VS_VEC_EYE_POS).sub_v(&pos_w).normalize();
 
-        self.out_vec3(&Vector3::new(view.x, view.y, view.z));
-        self.out_vec3(&Vector3::new(norm.x, norm.y, norm.z));
+        self.out_vec3_from4(&view);
+        self.out_vec3_from4(&norm);
         pos_pvw
     }
 
@@ -148,52 +153,50 @@ impl Shader {
     // 0 - Vector3 view
     // 3 - Vector3 norm
     #[allow(dead_code)]
-    pub fn pixel_phong_blinn(&mut self) -> Vector3<f32> {
+    pub fn pixel_phong_blinn(&self) -> Vector3<f32> {
         let view = Vector3::new(self.in_pixel_data[0], self.in_pixel_data[1], self.in_pixel_data[2]).normalize();
         let norm = Vector3::new(self.in_pixel_data[3], self.in_pixel_data[4], self.in_pixel_data[5]).normalize();
-        let light4 = self.read_vec4(IN_VS_VEC_NEG_LIGHT);
-        let light = Vector3::new(light4.x, light4.y, light4.z);
+        let light = self.read_vec3(IN_VS_VEC_NEG_LIGHT);
         let half = view.add_v(&light).normalize();
-        let cos_nh = norm.dot(&half);
-        let cos_nl = norm.dot(&light);
+        let cos_nh = norm.dot(&half).max(0.0_f32);
+        let cos_nl = norm.dot(&light).max(0.0_f32);
 
-        let spec_power = 5.0_f32;
+        const POWER: i32 = 5;
 
         let ambient = self.ambient.mul_s(self.ambient_intensity);
-        let diffuse = self.diffuse.mul_s(cos_nl.max(0.0_f32));
-        let specular = self.specular.mul_s(cos_nh.max(0.0_f32).powf(spec_power));
+        let diffuse = self.diffuse.mul_s(cos_nl);
+        let specular = self.specular.mul_s(cos_nh.powi(POWER));
 
         ambient + diffuse + specular
     }
 
     // out:
-    // 0 - Vector3 eye_vec
+    // 0 - Vector3 view
     // 3 - Vector3 norm
     #[allow(dead_code)]
     pub fn vertex_cook_torrance(&mut self) -> Vector4<f32> {
         let pos_pvw = self.matrix_arr[MATRIX_PROJ_VIEW_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_POS));
         let pos_w = self.matrix_arr[MATRIX_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_POS));
         let norm = self.matrix_arr[MATRIX_WORLD].mul_v(&self.read_vec4(IN_VS_VEC_NORM)).normalize();
-        let eye_vec = self.read_vec4(IN_VS_VEC_EYE_POS).sub_v(&pos_w).normalize();
+        let view = self.read_vec4(IN_VS_VEC_EYE_POS).sub_v(&pos_w).normalize();
 
-        self.out_vec3(&Vector3::new(eye_vec.x, eye_vec.y, eye_vec.z));
-        self.out_vec3(&Vector3::new(norm.x, norm.y, norm.z));
+        self.out_vec3_from4(&view);
+        self.out_vec3_from4(&norm);
         pos_pvw
     }
 
     // in:
-    // 0 - Vector3 eye_vec
+    // 0 - Vector3 view
     // 3 - Vector3 norm
     #[allow(dead_code)]
     pub fn pixel_cook_torrance(&self) -> Vector3<f32> {
         let view = Vector3::new(self.in_pixel_data[0], self.in_pixel_data[1], self.in_pixel_data[2]).normalize();
         let norm = Vector3::new(self.in_pixel_data[3], self.in_pixel_data[4], self.in_pixel_data[5]).normalize();
-        let light4 = self.read_vec4(IN_VS_VEC_NEG_LIGHT);
-        let light = Vector3::new(light4.x, light4.y, light4.z);
+        let light = self.read_vec3(IN_VS_VEC_NEG_LIGHT);
         let half = view.add_v(&light).normalize();
 
-        let roughness = 0.3_f32;
-        let roughness_sq = roughness * roughness;
+        const ROUGHNESS: f32 = 0.3_f32;
+        const ROUGHNESS_SQ: f32 = ROUGHNESS * ROUGHNESS;
         
         let cos_hn = half.dot(&norm).max(0.0000001_f32);
         let cos_hn_sq = cos_hn * cos_hn;
@@ -203,8 +206,8 @@ impl Shader {
 
         let geometric = 1.0_f32.min((2.0_f32 * cos_hn * cos_vn.min(cos_ln)) / cos_vh);
         let frenel = 1.0_f32 / (1.0_f32 + cos_vn);
-        let pow_val = (cos_hn_sq - 1.0_f32) / (roughness_sq * cos_hn_sq);
-        let d = pow_val.exp() / (4.0_f32 * roughness_sq * cos_hn_sq * cos_hn_sq);
+        let pow_val = (cos_hn_sq - 1.0_f32) / (ROUGHNESS_SQ * cos_hn_sq);
+        let d = pow_val.exp() / (4.0_f32 * ROUGHNESS_SQ * cos_hn_sq * cos_hn_sq);
         let k = (geometric * frenel * d) / (cos_vn * cos_ln + 0.0000001_f32);
 
         let ambient = self.ambient.mul_s(self.ambient_intensity);
