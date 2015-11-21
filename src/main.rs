7aed92152f72a399dc9ffe0b17ef49f2362ebde3
ReanGD <reangd@yandex.ru>
2015-11-21 15:36:20 +0300

    add auto rotate

--- src/main.rs
+++ src/main.rs
@@ -28,43 +28,41 @@ pub fn main() {
     let ind = 0;
     let mut material = Material::gold();
     let mut init_matrix = Matrix4::<f32>::one();
-    let mut add_angle = rad(2.0_f32 * std::f32::consts::PI / 180.0_f32);
+    let add_angle = rad(2.0_f32 * std::f32::consts::PI / 180.0_f32);
     let light = Vector3::new(1.0_f32, -1.0_f32, 1.0_f32);
     match ind {
         0 => {
             eye = Point3::new(0.0_f32, 0.7_f32, -1.1_f32);
-	        center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
+	          center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
             material = Material::silver();
-            add_angle = rad(0.0_f32);
             model = Loader3ds::load("ring.3ds").unwrap();
         },
         1 => {
             eye = Point3::new(0.0_f32, 0.0_f32, -10.1_f32);
-	        center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
+	          center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
             model = Loader3ds::load("tux.3ds").unwrap();
         },
         2 => {
             eye = Point3::new(0.0_f32, -2.0_f32, -4.1_f32);
-	        center = Point3::new(0.0_f32, -2.0_f32, 0.0_f32);
+	          center = Point3::new(0.0_f32, -2.0_f32, 0.0_f32);
             material = Material::monster_skin();
             init_matrix = Matrix4::from(Matrix3::from_angle_x(rad(-1.8_f32)));
             model = Loader3ds::load("monster.3ds").unwrap();
         },
         3 => {
             eye = Point3::new(0.0_f32, 0.0_f32, 0.5_f32);
-	        center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
-            add_angle = rad(0.0_f32);
+	          center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
             model = generate_plane().unwrap();
         },
         4 => {
             eye = Point3::new(0.0_f32, 0.0_f32, -2.0_f32);
-	        center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
+	          center = Point3::new(0.0_f32, 0.0_f32, 0.0_f32);
             model = generate_sphere(50).unwrap();
         },
 
         _ => return
     };
-	let up = Vector3::new(0.0_f32, 1.0_f32, 0.0_f32);
+	  let up = Vector3::new(0.0_f32, 1.0_f32, 0.0_f32);
 
     let mut angle = rad(0.0_f32);
 
